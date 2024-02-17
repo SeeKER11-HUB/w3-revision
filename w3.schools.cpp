5e93459aee813d1292d48f852346cdaf6ef6215c
Seeker <aryan.deshwal777@gmail.com>
2024-02-17 11:59:26 +0530

    Initial commit

---
+++ w3.schools.cpp
@@ -0,0 +1,227 @@
+#include<iostream>
+using namespace std;
+
+ 
+ 
+ 
+int 
+main () 
+ { 
+//Print Hello WOORLD 2 TIME WITH 2 NEW LINES
+/*
+comment out
+cout<<"HELLO WOORLD\n";
+cout<<"new line\n";/ 
+/int number = 1;
+cout<<number;
+return 0;/ 
+//VARIABLES
+/
+int myage=17;
+double mffloat=8.766;
+char yelebsdk='a';
+string terabaapkaname="PAPA";
+cout<<myage<<"\n";
+cout<<mffloat<<"\n";
+cout<<yelebsdk<<"\n";
+cout<<terabaapkaname<<"\n";
+/ 
+//Add 2 VARIABLES
+/int x=5;
+int y=6;
+int sum=x+y;
+cout<<sum;/ 
+/bsdk agar int baar nahi likhna to declare multiple variable use kar.
+or bsdk bina comma lagaya use mat karna/ 
+/int x=2,y=3,z=1;
+int sum=x+y+z;
+cout<<sum;/ 
+/ LETS MAKE A CALCULATOR NOW 
+   /* int x, y;
+
+int sum;
+
+
+cout <<"enter 1 no";
+
+cin >> x ;
+
+cout << "enter 2 no";
+
+cin >> y  ;
+
+sum = x + y;
+
+cout <<"the sum is"<<sum;*/ 
+// add 2 VARIABLES
+/*int x=100+50;
+cout<<x;*/ 
+//yw toh mujha bhi nahi pata kya bakchodi hai abhi neecha but
+//kuch toh hai
+/*
+int sum1=100+50;
+int sum2=sum1+50;
+int sum3=sum2+sum1;
+cout<<sum1<<"\n";
+cout<<sum2<<"\n";
+cout<<sum3<<"\n";*/ 
+//MULTIPLICATION
+/*	
+int sum1 = 100 * 50;
+  
+int sum2 = sum1 * 50;
+  
+int sum3 = sum2 * sum1;
+  
+cout << sum1 << "\n";
+  
+cout << sum2 << "\n";
+  
+cout << sum3 << "\n";
+*/ 
+/*
+int sum1 = 100 /50;
+  
+int sum2 = 200 / 50;
+  
+int sum3 = sum2 / sum1;
+  
+cout << sum1 << "\n";
+  
+cout << sum2 << "\n";
+  
+cout << sum3 << "\n";*/ 
+/*
+int a=5-6;
+cout<<a;*/ 
+//increment
+/*
+
+int a=10;
+int b=20;
+int c=30;
+int sum=a+b+c;
+cout<<sum<<"\n";
+sum++;
+cout<<sum;*/ 
+// addition assignment
+/*
+int x=100;
+
+x+=50;
+cout<<x;*/ 
+//comparison
+/*
+int x=5;
+int y=6;
+cout<<(x>y);
+*/ 
+//lets check 5==6
+/*
+int x=5;
+int y=5;
+cout<<(x==5);*/ 
+/*
+string character="pajiii";
+cout<<character;*/ 
+//brb-_-
+//string concentration
+/*sidhu prava
+ string firstname="sidhu";
+ string secondname="moosewala";
+ string completename=firstname+secondname;
+ cout<<completename;
+ */ 
+	//if we add two sting the result will be string concentration
+	/*
+	   string x="20";
+	   string y="30";
+	   string z=x+y;
+	   cout<<z; */ 
+	// ok time to do some maths
+	//have to add cm math header file(its neccesary)
+	/*
+	   cout<< sqrt(13)<<"\n";
+	   cout<<log(5);
+	 */ 
+//lets try boolean data type
+/*
+int x=56;
+int y=57;
+cout<<(x>y);
+*/ 
+//eligible to vote or not
+/*
+int myage=17;
+int votingage=18;
+cout<<(myage>=votingage);*/ 
+//if else statement
+/*
+int myage=17;
+int votingage=18;
+
+if(myage>=votingage)
+{cout<<"eligible for vote";}
+else
+{
+    cout<<"not eligible";
+}
+*/ 
+//executing else if statement;
+/*
+int age;
+cin>>age;
+if(age>=18)
+{
+    cout<<"elible";
+    
+}
+
+ else
+ 
+ {
+     cout<<"not eligible";
+ }
+ */ 
+	//the switch statement
+	/*
+int day=4;
+switch (day){
+case1:
+cout <<"monday";
+break;
+case2:
+cout <<"tuesday";
+break;
+case3:
+cout <<"wednesday";
+break;
+case4:
+cout << "thursday";
+break;
+case5:
+cout << "friday";
+break;
+case6:
+cout << "saturday";
+break;
+case7:
+cout << "sunday";
+break;
+}
+*/
+//while condition
+int sidhu=0;
+while(sidhu<10)
+{cout<<sidhu<<"\n";
+sidhu++;
+}
+
+return 0;
+}
+
+
+
+
+ 
+ 
